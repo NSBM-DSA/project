ec8a609c3d4e7282905ac1526da254345295198f
ishan de silva <ishantdesilva@gmail.com>
2015-04-02 23:30:04 +0530

    Ishan first commit

--- ListHouse.java
+++ ListHouse.java
@@ -1,14 +1,10 @@
 
 public class ListHouse {
-<<<<<<< HEAD
 	private String houseName;
 	private int houseLotNumber;
 	private String ownerFirstName;
 	private String ownerLastName;
 	private int price;
 	private int houseSquareFeet;
-	private int bedrooms;
-=======
-	///list house file
->>>>>>> 6213aae806ccd6ae6c01ae0d9a8a8c379d8f66ee
+	private int bedrooms; 
 }
