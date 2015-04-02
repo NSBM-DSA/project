47a5b24a3d2903e8dfba2ca509f1418ee1920bcc
bynulorenz <bynu022dev@gmail.com>
2015-04-03 00:08:14 +0530

    Getters Setters for SortedList

--- SortedList.java
+++ SortedList.java
@@ -1,4 +1,16 @@
+import java.util.LinkedList;
+
+
+
 
 public class SortedList {
-	// sorting class
+	private static LinkedList<ListHouse> sortedHouseList=new LinkedList<ListHouse>();
+
+	public LinkedList<ListHouse> getSortedHouseList() {
+		return sortedHouseList;
+	}
+
+	public void setSortedHouseList(LinkedList<ListHouse> sortedHouseList) {
+		SortedList.sortedHouseList = sortedHouseList;
+	}
 }
