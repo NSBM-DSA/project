e5678cc39875d94e9a0930ef6f22da1d8fe9bf4f
ishan de silva <ishantdesilva@gmail.com>
2015-04-03 00:17:20 +0530

    Adding variables

--- HouseFile.java
+++ HouseFile.java
@@ -1,8 +1,4 @@
 
 public class HouseFile {
-<<<<<<< HEAD
-	//House File
-=======
-	House File
->>>>>>> 6213aae806ccd6ae6c01ae0d9a8a8c379d8f66ee
+	LinkedList<ListHouse> houseList = new LinkedList<ListHouse>();
 }	
