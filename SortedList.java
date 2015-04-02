43f130bdc80a5e2f8eca9ced5142416f90c7d39e
ishan de silva <ishantdesilva@gmail.com>
2015-04-03 00:34:24 +0530

    changes SortedList.java

--- SortedList.java
+++ SortedList.java
@@ -1,10 +1,31 @@
 import java.util.LinkedList;
 
 public class SortedList {
+	private static LinkedList<ListHouse> houseList = new LinkedList<ListHouse>();
 	private static LinkedList<ListHouse> sortedHouseList=new LinkedList<ListHouse>();
 
+	public LinkedList<ListHouse> getHouseList() {
+		return houseList;
+	}
+
+	public void addHouseToHouseList(ListHouse house) {
+		if (SortedList.houseList == null) {
+			houseList = new LinkedList<ListHouse>();
+		} else {
+			SortedList.houseList.add(house);
+		}
+
+	}
+
+	public void clearHouseList() {
+		houseList = null;
+	}
+
+	public void setHouseList(LinkedList<ListHouse> house) {
+		SortedList.houseList = house;
+	}
 	public LinkedList<ListHouse> getSortedHouseList() {
-		return sortedHouseList; 
+		return sortedHouseList;
 	}
 
 	public void setSortedHouseList(LinkedList<ListHouse> sortedHouseList) {
