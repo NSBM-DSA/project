e8c7f77a6b90226dd10f8d22a9cca78528e27780
ishan de silva <ishantdesilva@gmail.com>
2015-04-03 00:04:27 +0530

    SortedList adding variable

--- SortedList.java
+++ SortedList.java
@@ -1,8 +1,4 @@
 
 public class SortedList {
-<<<<<<< HEAD
-	// sorting class
-=======
-	 sorting class
->>>>>>> 6213aae806ccd6ae6c01ae0d9a8a8c379d8f66ee
+	LinkedList<ListHouse> sortedHouseList=new LinkedList<ListHouse>();
 }
