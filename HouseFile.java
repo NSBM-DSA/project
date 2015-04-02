645dcab137bdcc68eac0317ac4849b126818edb0
ishan de silva <ishantdesilva@gmail.com>
2015-04-03 01:05:26 +0530

    constructor change HouseFile.java

--- HouseFile.java
+++ HouseFile.java
@@ -7,8 +7,15 @@ public class HouseFile {
 	
 	public HouseFile(int lot, String first, String last, int price, int feet,
 			int bed) {
-		
+		this.lotNumber = lot;
+		this.ownerFirstName = first;
+		this.ownerLastName = last;
+		this.housePrice = price;
+		this.houseSquareFeet = price;
+		this.houseBedrooms = bed;
+		house = new ListHouse();
 	}
+	
 	public HouseFile() {
 
 	}
