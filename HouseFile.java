00c5ea28a732ad3eb14f2ec29aeb9d3606255c6f
Anu <thilananupa@gmail.com>
2015-04-03 01:26:12 +0530

    add house function HouseFile.java

--- HouseFile.java
+++ HouseFile.java
@@ -1,4 +1,35 @@
 
 public class HouseFile {
-	LinkedList<ListHouse> houseList = new LinkedList<ListHouse>();
+	
+	public class HouseFile {
+
+	private int lotNumber, housePrice, houseSquareFeet, houseBedrooms;
+	private String ownerFirstName, ownerLastName;
+	private ListHouse house;
+	private final SortedList houseList = new SortedList();
+
+	public HouseFile(int lot, String first, String last, int price, int feet,
+			int bed) {
+		this.lotNumber = lot;
+		this.ownerFirstName = first;
+		this.ownerLastName = last;
+		this.housePrice = price;
+		this.houseSquareFeet = price;
+		this.houseBedrooms = bed;
+		house = new ListHouse();
+	}
+
+	public HouseFile() {
+
+	}
+
+	public void addHouse() {
+		house.setHouseLotNumber(lotNumber);
+		house.setOwnerFirstName(ownerFirstName);
+		house.setOwnerLastName(ownerLastName);
+		house.setPrice(housePrice);
+		house.setHouseSquareFeet(houseSquareFeet);
+		house.setBedrooms(houseBedrooms);
+	}
+}
 }	
